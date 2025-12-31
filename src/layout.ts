90804075f26ba64d6c00db687396ced64260dc42
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-12-31 18:46:37 +0200

    feat: layout: implement first walk shift calculation;

--- src/layout.ts
+++ src/layout.ts
@@ -1,6 +1,6 @@
 import { Node } from '@xyflow/react';
 
-import { Index } from 'model';
+import { Index, Marriage } from 'model';
 
 const NODE_WIDTH = 100;
 const NODE_HEIGHT = 40;
@@ -39,7 +39,91 @@ type PreNode = {
     shift: number;
 };
 
-function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, PreNode>, preX: number): PreNode {
+function getParent(id: Id, family: Index, selectParent: (marriage: Marriage) => string): Id | null {
+    let parentId: string;
+
+    if (id.type === MARRIAGE_TYPE) {
+        const marriage = family.marriageById.get(id.id);
+        if (!marriage) {
+            throw new Error(`Expected marriage to exist for id ${id.id}`);
+        }
+
+        parentId = selectParent(marriage);
+    } else {
+        // Currently, this case is not supported, but let's handle it.
+        parentId = id.id;
+    }
+
+    const parents = family.personParents.get(parentId);
+
+    if (!parents) {
+        return null;
+    } else {
+        return { type: MARRIAGE_TYPE, id: parents };
+    }
+}
+
+function getRightmostParent(id: Id, family: Index): Id | null {
+    const selectRightParent = (marriage: Marriage) => {
+        if (marriage.parent2_id) {
+            return marriage.parent2_id;
+        } else if (marriage.parent1_id) {
+            return marriage.parent1_id;
+        } else {
+            throw new Error(`Expected at least one parent to exist for marriage id ${marriage.id}`);
+        }
+    };
+
+    return getParent(id, family, selectRightParent);
+}
+
+function getLeftmostParent(id: Id, family: Index): Id | null {
+    const selectLeftParent = (marriage: Marriage) => {
+        if (marriage.parent1_id) {
+            return marriage.parent1_id;
+        } else if (marriage.parent2_id) {
+            return marriage.parent2_id;
+        } else {
+            throw new Error(`Expected at least one parent to exist for marriage id ${marriage.id}`);
+        }
+    };
+
+    return getParent(id, family, selectLeftParent);
+}
+
+function calculateShift(siblingLeft: Id, leftShift: number, singlingRight: Id, rightShift: number, preNodes: Map<string, PreNode>, family: Index): number {
+    const leftNode = preNodes.get(siblingLeft.id);
+    if (!leftNode) {
+        throw new Error(`Expected left sibling pre-node to exist for id ${siblingLeft.id}`);
+    }
+
+    const rightNode = preNodes.get(singlingRight.id);
+    if (!rightNode) {
+        throw new Error(`Expected right sibling pre-node to exist for id ${singlingRight.id}`);
+    }
+
+    const leftX = leftNode.x + leftShift + leftNode.shift;
+    const rightX = rightNode.x + rightShift + rightNode.shift;
+
+    let shift = 0;
+    if (rightX - leftX < NODES_GAP) {
+        shift = NODES_GAP - (rightX - leftX);
+    }
+
+    leftShift += leftNode.mod + leftNode.shift;
+    rightShift += rightNode.mod + rightNode.shift;
+
+    const nextLeftSibling = getRightmostParent(siblingLeft, family);
+    const nextRightSibling = getLeftmostParent(singlingRight, family);
+
+    if (!nextLeftSibling || !nextRightSibling) {
+        return shift;
+    }
+
+    return Math.max(shift, calculateShift(nextLeftSibling, leftShift, nextRightSibling, rightShift, preNodes, family));
+}
+
+function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, PreNode>, preX: number, siblings: Id[]): PreNode {
     let parents: Id[] = [];
     if (perspectiveId.type === PERSON_TYPE) {
         const marriageId = family.personParents.get(perspectiveId.id);
@@ -90,7 +174,7 @@ function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, P
             throw new Error('should not be possible');
         }
 
-        const parentNode = buildPreNodes(parent, family, preNodes, 0);
+        const parentNode = buildPreNodes(parent, family, preNodes, 0, [parent]);
 
         // Now we need to do different actions depending on whether the current node is a left-most node or not.
         // We do that with a simple trick: the current node is the left-most node if its `preX` is equal to 0.
@@ -114,6 +198,15 @@ function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, P
 
         preNodes.set(perspectiveId.id, preNode);
 
+        for (const sibling of siblings) {
+            if (perspectiveId === sibling) {
+                break;
+            }
+
+            const shift = calculateShift(sibling, 0, perspectiveId, 0, preNodes, family);
+            preNode.shift += shift;
+        }
+
         return preNode;
     }
 
@@ -127,7 +220,7 @@ function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, P
         throw new Error('should not be possible: second parent must present');
     }
 
-    let firstPreNode = buildPreNodes(firstParent, family, preNodes, 0);
+    let firstPreNode = buildPreNodes(firstParent, family, preNodes, 0, parents);
     let deltaX: number;
     if (firstPreNode.id.type === PERSON_TYPE) {
         deltaX = NODE_WIDTH + NODES_GAP;
@@ -135,7 +228,7 @@ function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, P
         deltaX = MARRIAGE_WIDTH + NODES_GAP;
     }
 
-    let secondPreNode = buildPreNodes(secondParent, family, preNodes, deltaX);
+    let secondPreNode = buildPreNodes(secondParent, family, preNodes, deltaX, parents);
 
     // Now we need to do different actions depending on whether the current node is a left-most node or not.
     // We do that with a simple trick: the current node is the left-most node if its `preX` is equal to 0.
@@ -159,6 +252,15 @@ function buildPreNodes(perspectiveId: Id, family: Index, preNodes: Map<string, P
 
     preNodes.set(perspectiveId.id, preNode);
 
+    for (const sibling of siblings) {
+        if (perspectiveId === sibling) {
+            break;
+        }
+
+        const shift = calculateShift(sibling, 0, perspectiveId, 0, preNodes, family);
+        preNode.shift += shift;
+    }
+
     return preNode;
 }
 
@@ -176,7 +278,7 @@ export function buildNodes(perspectiveId: string, family: Index): Node[] {
         id = { type: PERSON_TYPE, id: perspectiveId };
     }
 
-    const rootPreNode = buildPreNodes(id, family, preNodes, 0);
+    const rootPreNode = buildPreNodes(id, family, preNodes, 0, [id]);
 
     return [];
 }
