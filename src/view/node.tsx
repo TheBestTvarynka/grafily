86c7eea65b2ad1ab6eee24f9aae67c62e4016c53
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2026-07-05 18:38:40 +0300

    fix(ui): do not render marriage node button if marriage has no children;

--- src/view/node.tsx
+++ src/view/node.tsx
@@ -333,7 +333,7 @@ export function MarriageNode({ data }: { data: MarriageNodeData }) {
                 position: 'relative',
             }}
         >
-            {data.isChildrenCollapsible ? (
+            {data.isChildrenCollapsible && hasChildren ? (
                 <button
                     onClick={collapseChildren}
                     style={{
